18f9d40786fca712f5c0369e5f9635a4abdb6ded
mamesareagan <mamesareagan0@gmail.com>
2025-03-28 12:41:34 -0400

    Update ALLOWED_HOSTS to include production domain for deployment

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='csv-backend-psi.vercel.app', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 INSTALLED_APPS = [
